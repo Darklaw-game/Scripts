073e7a7aa1db6eefadf452dca1c1ff4653c4bb94
Darklaw-game <darklaw.game@gmail.com>
2024-12-28 10:11:43 -0600

    Create Test.lua

---
+++ Test.lua
@@ -0,0 +1,9 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Testing", "Midnight")
+local Tab = Window:NewTab("Local")
+Section:NewButton("ButtonText", "ButtonInfo", function()
+    print("Clicked")
+end)
+Section:NewSlider("SliderText", "SliderInfo", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
