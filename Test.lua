cd8e735d42fbbc5d2de21f133b25a684c4bc09c2
Darklaw-game <darklaw.game@gmail.com>
2024-12-28 10:46:52 -0600

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -3,5 +3,9 @@ local Window = Library.CreateLib("Testing", "Midnight")
 
 
 --main
-local Main = Window:NewTab("Mainh")
+local Main = Window:NewTab("Local")
 local MainSection = Main:NewSection("Main")
+
+MainSection:NewSlider("SliderText", "SliderInfo", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
