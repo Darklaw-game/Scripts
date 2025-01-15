4bb0bb0fa0ece7040284802fc4c53fd295128153
Darklaw-game <darklaw.game@gmail.com>
2025-01-14 20:49:23 -0600

    Update Doors.lua

--- Doors.lua
+++ Doors.lua
@@ -47,12 +47,16 @@ Rayfield:Notify({
 local Button = MainTab:CreateButton({
    Name = "Infinite Jump Toggle",
    Callback = function()
-       https://raw.githubusercontent.com/Darklaw-game/Scripts/refs/heads/main/invis.lua
-   end,
-			end
-		end
-	end)
-end
+mouse = game.Players.LocalPlayer:GetMouse()
+tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = "TP TOOL"
+tool.Activated:connect(function()
+local pos = mouse.Hit+Vector3.new(0,2.5,0)
+pos = CFrame.new(pos.X,pos.Y,pos.Z)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
+end)
+tool.Parent = game.Players.LocalPlayer.Backpack
    end,
 })
 
