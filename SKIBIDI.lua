2d6ff6f896ab7bd3acce5d8c6a0a2861be7e49ba
Darklaw-game <darklaw.game@gmail.com>
2025-01-14 20:54:46 -0600

    Update SKIBIDI.lua

--- SKIBIDI.lua
+++ SKIBIDI.lua
@@ -45,6 +45,24 @@ Rayfield:Notify({
 })
 
 
+local Button = MainTab:CreateButton({
+   Name = "Button Example",
+   Callback = function()
+   -- The function that takes place when the button is pressed
+         mouse = game.Players.LocalPlayer:GetMouse()
+tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = "TP TOOL"
+tool.Activated:connect(function()
+local pos = mouse.Hit+Vector3.new(0,2.5,0)
+pos = CFrame.new(pos.X,pos.Y,pos.Z)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
+end)
+tool.Parent = game.Players.LocalPlayer.Backpack
+   end,
+})
+
+
 local Slider = MainTab:CreateSlider({
    Name = "JumpPower Slider",
    Range = {1, 350},
